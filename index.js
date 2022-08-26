de785de29ad21c82857f3fce818c5c74a44b3451
eli-codes <elitutor0116@gmail.com>
2022-08-25 17:53:25 -0700

    Completed Tasks 1 & 2

--- index.js
+++ index.js
@@ -45,11 +45,10 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-
-function copy(/*your code here*/){
-  /*your code here*/
-}
-
+  function copy(arr) {
+    return [...arr];
+  }
+  console.log('task 1:', copy(originalFlavors));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -63,8 +62,12 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-  /*your code here*/
+function is31Flavors(arr){
+  if(arr.length === 31) {
+    return true
+    } else {
+      return false
+    }
  }
 
 
